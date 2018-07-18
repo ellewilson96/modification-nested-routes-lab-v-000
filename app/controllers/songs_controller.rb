e0202008d9230a85369920060780e70e62fee4be
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-18 18:50:28 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,6 +15,9 @@ class SongsController < ApplicationController
   def show
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+        redirect_to edit_artist_song_path(@artist, song), alert "Add Artist"
+      else
       @song = @artist.songs.find_by(id: params[:id])
       if @song.nil?
         redirect_to artist_songs_path(@artist), alert: "Song not found"
